3ec9ff82980f617f81532e26e187c34ba118dc18
mirkiy <mirizachar@gmail.com>
2023-06-29 17:53:20 +0100

    img displaying

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -13,7 +13,7 @@ const ListItem = ({ product }) => {
     <div className="row row-cols-1 row-cols-md-2 g-4">
       <div className="col">
         <div className="card">
-          <img src="{product.image_link}" className="card-img-top" alt="..." />
+          <img src={product.image_link} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
             <p className="card-text">
